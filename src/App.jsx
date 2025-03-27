7631ec30cd5ac225b510fa1455c43fa0816f2307
maniadiaz <mania518alexis@gmail.com>
2025-03-26 19:36:14 -0700

    se agrego una tabla en nuestro proyecto react

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const usuarios = [
+    { nombre: "Juan", apellido: "Perez", edad: 30 },
+    { nombre: "Ana", apellido: "Gomez", edad: 25 },
+    { nombre: "Carlos", apellido: "Lopez", edad: 40 },
+    { nombre: "Katherine", apellido: "Guardado", edad: 17 },
+    { nombre: "Alexis", apellido: "Diaz", edad: 17 }
+  ];
 
   return (
     <>
       <div className="card">
         <button onClick={() => {
-          if(count > 10){
-            alert("Llegaste al limite");
+          if(count < 10){
+            setCount(count + 1);
+          }else{
+            alert("Llegaste al límite");
           }
-          setCount(count + 1)
-        } }>
-          count is {count}
+          
+        }}>
+          Contador es {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
 
+      <div className='card'>
+        <h1>Usuarios</h1>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Edad</th>
+            </tr>
+          </thead>
+          <tbody>
+            {usuarios.map((usuario, index) => (
+              <tr key={index}>
+                <td>{usuario.nombre}</td>
+                <td>{usuario.apellido}</td>
+                <td>{usuario.edad}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
